4d6b5b9b851284b884e2b5a92729b9a942e457db
Sean Kelly <spkelly18@gmail.com>
2019-03-11 12:53:40 -0600

    Added migration file for users and added modifications to routes

--- routes/user.js
+++ routes/user.js
@@ -3,14 +3,32 @@ var router = express.Router();
 
 
 router.post("/login", (req,res,next)=>{
+  let username = req.body.username;
+  let password = req.body.password;
+  
+  if(username.length < 0 || password.length < 0){
+    let errorMessage = 'Username or Password is invalid';
+    next(errorMessage);
+  }
+
+  // Verify user from database
+  // if verification passes send 200 and Jwt token Header 
+  // if verification fails send 400 error
   res.send('login endpoint');
 });
+
+
+
 router.get("/logout", (req,res,next)=>{
   res.send('logout endpoint');
 });
+
+// TODO: Write JWT token middleware to access protected endpoints
 router.get("/profile", (req,res,next)=>{
   res.send('profile endpoint');
 });
+
+
 router.get("/", (req,res,next)=>{
   res.send('get all users endpoint');
 });
