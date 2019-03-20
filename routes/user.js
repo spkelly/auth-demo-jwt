87521c97c799cf0d684308c7e37b8c591c3c2af3
spkelly <spkelly18@gmail.com>
2019-03-20 14:38:36 -0600

    Added JWT signing to authenticaiton route

--- routes/user.js
+++ routes/user.js
@@ -1,35 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('jsonwebtoken');
 
+router.post('/login', (req, res, next)=>{
+  const username = req.body.username;
+  const password = req.body.password;
 
-router.post("/login", (req,res,next)=>{
-  let username = req.body.username;
-  let password = req.body.password;
-  
-  if(username.length < 0 || password.length < 0){
-    let errorMessage = 'Username or Password is invalid';
+  if(!username|| !password){
+    const errorMessage = 'Username or Password is invalid';
     next(errorMessage);
   }
-
+  const token = jwt.sign({username, password},
+    process.env.JWT_SECRET,
+    {});
   // Verify user from database
-  // if verification passes send 200 and Jwt token Header 
+  // if verification passes send 200 and Jwt token Header
   // if verification fails send 400 error
-  res.send('login endpoint');
+  res.json({_token:token});
 });
 
 
 
-router.get("/logout", (req,res,next)=>{
+router.get('/logout', (req, res, next)=>{
   res.send('logout endpoint');
 });
 
 // TODO: Write JWT token middleware to access protected endpoints
-router.get("/profile", (req,res,next)=>{
+router.get('/profile', (req, res, next)=>{
   res.send('profile endpoint');
 });
 
 
-router.get("/", (req,res,next)=>{
+router.get('/', (req, res, next)=>{
   res.send('get all users endpoint');
 });
 
