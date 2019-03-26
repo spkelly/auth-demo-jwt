71ea7264d600fac9c94440ffda8f8f916881307d
spkelly <spkelly18@gmail.com>
2019-03-26 13:23:50 -0600

    linting

--- migrations/20190311124312_users.js
+++ migrations/20190311124312_users.js
@@ -1,13 +1,13 @@
 
 exports.up = function(knex) {
-  return knex.schema.createTable('users',(table)=>{
+  return knex.schema.createTable('users', (table)=>{
     table.increments('user_id').notNull().primary();
     table.string('display_name').notNull();
     table.string('image_url');
     table.string('email').unique().notNull();
     table.string('hash').notNull();
     table.timestamps();
-  })
+  });
 };
 
 exports.down = function(knex) {
