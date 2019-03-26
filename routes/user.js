5bb446e15b6018b7494451cd22213e3a7f8a2960
spkelly <spkelly18@gmail.com>
2019-03-26 14:02:25 -0600

    added bcrypt password comparison on sign in

--- routes/user.js
+++ routes/user.js
@@ -1,22 +1,39 @@
 var express = require('express');
 var router = express.Router();
 var jwt = require('jsonwebtoken');
-
+var bcrypt = require('bcryptjs');
+var knex = require('../knex');
 router.post('/login', (req, res, next)=>{
-  const username = req.body.username;
-  const password = req.body.password;
 
-  if(!username|| !password){
-    const errorMessage = 'Username or Password is invalid';
-    next(errorMessage);
-  }
-  const token = jwt.sign({username, password},
-    process.env.JWT_SECRET,
-    {});
-  // Verify user from database
-  // if verification passes send 200 and Jwt token Header
-  // if verification fails send 400 error
-  res.json({_token:token});
+  setTimeout(()=>{
+    const email = req.body.username;
+    const password = req.body.password;
+    if(!email|| !password){
+      const errorMessage = 'Username or Password is invalid';
+      next(errorMessage);
+    }
+    knex('users').where({email})
+    .select(['user_id', 'hash'])
+    .then((data)=>{
+      if(data.length > 0){
+        const token = jwt.sign({
+          user_id: data[0].user_id},
+          process.env.JWT_SECRET, {}
+        );
+        res.json({_token:token});
+      }
+      else{
+        const errorMessage = 'Username or Password is invalid';
+        next(errorMessage);
+      }
+    })
+    .catch((err)=>{
+      res.json({err:'an error hass occured'});
+    });
+    // Verify user from database
+    // if verification passes send 200 and Jwt token Header
+    // if verification fails send 400 error
+  }, 1000);
 });
 
 
