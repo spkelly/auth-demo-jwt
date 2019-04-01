a54c16a73e5e7481dbe04f7d93d07a1bc2206df1
spkelly <spkelly18@gmail.com>
2019-04-01 09:18:12 -0600

    configured CORS to work with allow requests from a local environment

--- server.js
+++ server.js
@@ -2,25 +2,29 @@ require('dotenv').config();
 
 var express = require('express');
 var bodyParser = require('body-parser');
-var userRoutes = require("./routes/user");
-const path = require("path");
+var userRoutes = require('./routes/user');
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
-
+const port = process.env.PORT || 3001;
 
 app.set('view engine', 'pug');
 app.set('x-powered-by', false);
-app.set('views', path.resolve(__dirname,"views"));
-app.use(bodyParser.urlencoded({extended:false}));
-app.use("/users",userRoutes);
+app.set('views', path.resolve(__dirname, 'views'));
+app.use(bodyParser.json());
+app.all('/*', (req, res, next) =>{
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', ['Content-Type', 'Authorization']);
+  next();
+});
+app.use('/users', userRoutes);
 app.use(errorHandler);
 
-app.get('*', (req,res,next)=>{
+app.get('*', (req, res, next)=>{
   res.sendStatus(200);
 })
 
-function errorHandler(err, req,res,next){
-  res.send(`Error: ${err}`);
+function errorHandler(err, req, res, next){
+  res.json({err});
 }
 
 
